c3bb240491a78bd37476f60c7274420cb2374cb6
andreas <andreasyny@gmx.ca>
2024-07-26 21:16:26 +0700

    chore: Add autopurge command for managing channel purging jobs

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,11 +1,14 @@
-import { Client, GatewayIntentBits, BaseGuildTextChannel, ChannelType } from 'discord.js';
-import registerCommands from './Commands';
+import { Client, GatewayIntentBits, BaseGuildTextChannel, ChannelType, GuildMember } from 'discord.js';
 import { Config } from "./Config";
+import purgeChannel ,{ addPurgeJob, removePurgeJob } from './Modules/AutoPurge';
+import registerCommands from './Commands';
+import cronInit from './Cron';
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, 'GuildModeration'] });
+export const client = new Client({ intents: [GatewayIntentBits.Guilds, 'GuildModeration', 'GuildMessages'] });
 
 client.on('ready', () => {
     new Promise((resolve, reject) => {
+        cronInit()
         registerCommands().then(() => resolve('done')).catch(reject)
     }).then(e => console.log('Bot is here'))
 })
@@ -14,16 +17,35 @@ client.on('interactionCreate', async interaction => {
     if (!interaction.isChatInputCommand()) return;
 
     if (interaction.commandName === "about") {
-        await interaction.reply('I know who you are')
+        purgeChannel(client, {channelId:interaction.channelId})
+        await interaction.reply("idk if that work")
     }
 
-    if (interaction.commandName === "prune") {
+    if (interaction.commandName === "purge") {
         const collector = await interaction.channel?.messages.fetch()
         if (interaction.channel?.type === ChannelType.GuildText) {
             const textChannel = interaction.channel as BaseGuildTextChannel;
             await textChannel.bulkDelete(collector!.size);
         }
-        await interaction.reply(`Pruned ${collector?.size} messages`)
+        await interaction.reply(`Purged ${collector?.size} messages`)
+    }
+
+    if (interaction.commandName === "setautopurge") {
+        const mem = interaction.member as GuildMember
+        if (!mem.permissions.has("ManageMessages")) {
+            await interaction.reply("You don't have the required permissions")
+        } else {
+            addPurgeJob(interaction, {channelId: interaction.channelId})
+        }
+    }
+
+    if (interaction.commandName === "unsetautopurge") {
+        const mem = interaction.member as GuildMember
+        if (!mem.permissions.has("ManageMessages")) {
+            await interaction.reply("You don't have the required permissions")
+        } else (
+            removePurgeJob(interaction, {channelId: interaction.channelId})
+        )
     }
 })
 
