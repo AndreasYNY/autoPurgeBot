da9034b5f8c8f4929c540d65543c988e24ebf68c
andreas <andreasyny@gmx.ca>
2024-07-26 21:16:17 +0700

    chore: Add jobs.json file for storing cron jobs

--- src/Config.ts
+++ src/Config.ts
@@ -1,4 +1,6 @@
 import dotenv from "dotenv"
+import { join }from 'path';
+import fs from 'fs';
 dotenv.config()
 
 interface config {
@@ -11,6 +13,20 @@ if(!process.env.DISCORD_TOKEN || !process.env.CLIENT_ID || !process.env.APP_ID) 
     console.error("Missing environment variable(s)")
     process.exit(1)
 }
+
+export const jobsFile = join('cronStuff',"jobs.json");
+
+try {
+    const test = JSON.parse(fs.readFileSync(jobsFile).toString());
+} catch (error) {
+    if (error instanceof SyntaxError) {
+        console.error("Error parsing JSON file");
+        fs.writeFileSync(jobsFile, JSON.stringify([]));
+    }
+}
+
+export const jobs:{channelId: string, lastRun: number | null}[] = JSON.parse(fs.readFileSync(jobsFile).toString());
+
 
 export const Config: config = {
     TOKEN: process.env.DISCORD_TOKEN!,
